9409deedd7da5501a81eebab881297fbeb496edc
Rowleks <momohrowland@gmail.com>
2024-07-26 13:38:37 +0100

    "added dynamic order page"

--- src/components/order-cards/OrderCards.jsx
+++ src/components/order-cards/OrderCards.jsx
@@ -1,38 +1,50 @@
 import "./ordercards.scss";
+import PropTypes from "prop-types";
+import { orderBurger, orderPizza, orderShawarma } from "../../data";
 
-function OrderCards() {
+OrderCards.propTypes = {
+  selectedSnack: PropTypes.string,
+};
+
+function OrderCards({ selectedSnack }) {
+  let items = [];
+
+  // Determine which items to display based on selectedSnack
+  switch (selectedSnack) {
+    case 'pizza':
+      items = orderPizza;
+      break;
+    case 'burger':
+      items = orderBurger;
+      break;
+    case 'shawarma':
+      items = orderShawarma;
+      break;
+    default:
+      items = [];
+  }
+
   return (
     <div className="order-cards">
-    
       <div className="often-order">
-            <p>Often Ordered</p>
-            <span>See all</span>
-        </div>
+        <p>Often Ordered</p>
+        <span>See all</span>
+      </div>
 
-        <div className="order-card">
-            <div className="card">
-
-            <img src="pizza-pic1.png" alt="" />
+      <div className="order-card">
+        {items.map((item, index) => (
+          <div className="card" key={index}>
+            <img src={item.image} alt={item.name} />
             <div className="info-section">
-            <p>Extravaganza Pizza</p>
-            <span>N5,000</span>
-
-            <img className="cart" src="/cart-icon1.png" alt="" />
-
+              <p>{item.name}</p>
+              <span>N{item.price}</span>
+              <img className="cart" src="/cart-icon1.png" alt="Cart" />
             </div>
-
-            </div>
-            <div className="card">
-            <img src="/pizza-pic2.png" alt="" />
-            <div className="info-section">
-            <p>Chicken Pizza</p>
-            <span>N5,000</span>
-            <img className="cart" src="/cart-icon1.png" alt="" />
-            </div>
-            </div>
+          </div>
+        ))}
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
 export default OrderCards;
--- src/pages/homepage/Home.jsx
+++ src/pages/homepage/Home.jsx
@@ -1,29 +1,34 @@
-import ImageSlider from "../../components/image-slider/ImageSlider"
-import MenuTab from "../../components/menu-tab/MenuTab"
-import Navbar from "../../components/navbar/Navbar"
-import OrderSection from "../../components/order-section/OrderSection"
-import "./home.scss"
+import { useState } from "react";
+import ImageSlider from "../../components/image-slider/ImageSlider";
+import MenuTab from "../../components/menu-tab/MenuTab";
+import Navbar from "../../components/navbar/Navbar";
+import Navmenu from "../../components/navmenu/Navmenu";
+import OrderSection from "../../components/order-section/OrderSection";
+import "./home.scss";
 
 function Home() {
+  const [selectedSnack, setSelectedSnack] = useState(null);
+
   return (
     <div className="home">
-
-        <Navbar />
-        
+      <Navbar />
+      
       <div className="view">
-
         <div className="search">
-            <img src="/search-icon.png" alt="" />
-            <input type="text" placeholder="Search for favourite pizza" />
+          <img src="/search-icon.png" alt="" />
+          <input type="text" placeholder="Search for favourite pizza" />
         </div>
-
-            <ImageSlider />
-            <MenuTab />
-            <OrderSection />
-
+        
+        <ImageSlider />
+        <MenuTab selectedSnack={selectedSnack} setSelectedSnack={setSelectedSnack} />
+        <OrderSection selectedSnack={selectedSnack} />
+      </div>
+      
+      <div className="fixed-menu">
+        <Navmenu />
       </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
